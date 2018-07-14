9928f215eec6435d0be1ab463bb9059c79153fb8
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-07-14 23:47:09 +0900

    Add resources

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,169 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
+    <div class="header">
+      <span class="header-item title">Vue-8-Puzzle</span>
+      <span class="header-item distance"> d: {{distance}} </span>
+      <span class="header-item">
+        <label for="showNumber">#:</label>
+        <input type="checkbox" id="showNumber" v-model="showNumber" />
+      </span>
+      <span class="header-item">
+        <label for="animation">a:</label>
+        <input type="checkbox" id="animation" v-model="animation" />
+      </span>
+      <span class="header-item">
+        <select v-model="videoTitle">
+          <option>Cat</option>
+          <option>Penguin</option>
+        </select>
+      </span>
+      <span class="header-item">
+        <select v-model="difficulty">
+          <option>Easy</option>
+          <option>Normal</option>
+          <option>Difficult</option>
+        </select>
+      </span>
+      <span class="header-item">
+        <template v-if="isGoal">
+          finish!!
+        </template>
+      </span>
+    </div>
+    <div class="container">
+      <puzzle-board
+      :autoResize="autoResize"
+      :showNumber="showNumber"
+      :dx="dimensions.x"
+      :dy="dimensions.y"
+      :sources="sourcePath"
+      :animation="animation"
+      @init="onPuzzleBoardInit"
+      @start="onPuzzleBoardStart"
+      @change="onPuzzleBoardChange"
+      @finish="onPuzzleBoardFinish"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import PuzzleBoard from './components/PuzzleBoard'
+import PuzzleBoard from 'vue-8-puzzle'
+import penguinwebm from './assets/penguin.webm'
+import penguinmp4 from './assets/penguin.mp4'
+import catwebm from './assets/cat.webm'
+import catmp4 from './assets/cat.mp4'
 
+const SOURCEPATHS = {
+  Cat: [{
+    src: catwebm,
+    type: 'video/webm'
+  }, {
+    src: catmp4,
+    type: 'video/mp4'
+  }],
+  Penguin: [{
+    src: penguinwebm,
+    type: 'video/webm'
+  }, {
+    src: penguinmp4,
+    type: 'video/mp4'
+  }]
+}
+const DIMENSIONS = {
+  Easy: {x: 3, y: 3},
+  Normal: {x: 4, y: 4},
+  Difficult: {x: 5, y: 5}
+}
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    Hello
+    PuzzleBoard
+  },
+  data () {
+    return {
+      videoTitle: 'Cat',
+      difficulty: 'Easy',
+      distance: null,
+      isGoal: false,
+      autoResize: true,
+      showNumber: false,
+      animation: true
+    }
+  },
+  computed: {
+    sourcePath () {
+      return SOURCEPATHS[this.videoTitle]
+    },
+    dimensions () {
+      return DIMENSIONS[this.difficulty]
+    }
+  },
+  methods: {
+    onPuzzleBoardInit () {
+      console.log('init')
+    },
+    onPuzzleBoardStart () {
+      console.log('start')
+    },
+    onPuzzleBoardFinish () {
+      console.log('finish')
+      this.isGoal = true
+    },
+    onPuzzleBoardChange (payload) {
+      console.log('change')
+      this.distance = payload.distance
+    }
   }
 }
 </script>
 
 <style>
-body {
+html,body {
   margin: 0;
+  padding: 0;
+  background-color: #F0F0F0;
 }
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
-
-main {
-  text-align: center;
-  margin-top: 40px;
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  font-size: 1.4em;
+  position: absolute;
+  width: 100%;
+  height: 60px;
+  color: #FAFAFA;
+  background-color: #283593;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 }
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
+.header-item {
+  flex-grow: 1;
 }
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+.title {
+  font-weight: bold;
+}
+.container {
+  position: absolute;
+  top: calc(50% + 30px);
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 2px 4px rgba(0,0,0,0.16), 0 2px 4px rgba(0,0,0,0.23);
+  width: 100%;
+  height: calc(100% - 60px);
+  margin: 0px;
+  padding: 0px;
+  max-width: 600px;
+  max-height: 600px;
+  background-color: #DDD;
 }
 </style>
